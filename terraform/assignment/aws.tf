40160cde1a11c7fb8a33d37abb2bcd1d9704a9a5
Samanthadon <samantha.dawn.warren@gmail.com>
2025-10-15 15:21:05 -0500

    update s3 key
    update terraform s3 key

--- terraform/assignment/aws.tf
+++ terraform/assignment/aws.tf
@@ -8,7 +8,7 @@ data "aws_caller_identity" "current" {}
 terraform {
   backend "s3" {
     bucket         = "nmd-training-tf-states-706146613458"
-    key            = "assignment/samantha-eidson-nmd-data-engineering.tfstate"
+    key            = "assignment/nmd-assignment-samantha-eidson.tfstate"
     region         = "us-west-2"
     dynamodb_table = "nmd-training-tf-state-lock-table"    
     encrypt        = true                   # Encrypts the state file at rest
